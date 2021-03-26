bffeab65483ba39107f01871096e674582f1efaa
sanchesrfl <60938758+sanchesrfl@users.noreply.github.com>
2021-03-25 23:37:27 -0300

    third commit

--- etl/lambda_function.py
+++ etl/lambda_function.py
@@ -1,12 +1,24 @@
 import boto3
 import os
 
+file_name = "/tmp/titanic.csv"
+object_name = "raw-data/titanic/titanic.csv"
+
+def get_data():
+    url = "https://raw.githubusercontent.com/neylsoncrepalde/titanic_data_with_semicolon/main/titanic.csv"
+    urllib.request.urlretrieve(url, file_name)
+    return True
+
 def handler(event, context):
 
     bucket_name = os.environ['BUCKET']
     region = os.environ['REGION']
+    res = get_data()
+
+    s3 = boto3.client("s3")
+    s3.upload_file(file_name, bucket_name, object_name)
 
     return {
         'statusCode': 200,
-        'body': f'Hello World with bucket {bucket_name} on region {region}!'
+        'body': f'File {object_name} was uploaded to {bucket_name}, on region {region} xD'
     }
